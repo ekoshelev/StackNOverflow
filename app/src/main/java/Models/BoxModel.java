e2f0fb9f278674397e79ce69502b0ca1e0179c4b
Flavio Andrade <fandrade@wayfair.com>
2018-07-22 13:00:37 -0400

    Add method to get bigger dimensions of any two boxes.

--- app/src/main/java/Models/BoxModel.java
+++ app/src/main/java/Models/BoxModel.java
@@ -3,6 +3,8 @@ package Models;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class BoxModel implements Comparator<BoxModel> {
     private final String HEIGHT = "Height";
@@ -25,6 +27,13 @@ public class BoxModel implements Comparator<BoxModel> {
         this.length = length;
         this.volume = height * length * width;
 
+    }
+    public int getOrientation() {
+        return this.currentOrientation;
+    }
+
+    public void setOrientation(int orientation) {
+        this.currentOrientation = orientation;
     }
 
     public int getHeight() {
@@ -64,5 +73,39 @@ public class BoxModel implements Comparator<BoxModel> {
             return 1;
         }
         return 0;
+    }
+
+    // get the bigger dimensions between two boxes.
+    public ArrayList<String> getBiggerDimensions(BoxModel boxModel) {
+        int height = boxModel.getHeight();
+        int width = boxModel.getWidth();
+        int length = boxModel.getLength();
+        // holds the string name of the bigger dimension.
+        ArrayList<String> maxDimensions = new ArrayList<String>();
+        // if volume of this box is greater than the other box, no need to compare
+        // dimensions since this box should go in the container first.
+        if (this.volume > boxModel.getVolume()) {
+            return maxDimensions;
+        }
+
+        // if the volume of this box is equal to the other box,
+        // make sure the orientations are the same before comparing dimensions.
+        if (this.volume == boxModel.getVolume()) {
+            if (this.currentOrientation != boxModel.getOrientation()) {
+                boxModel.setOrientation(this.currentOrientation);
+            }
+            // even though the volume of this box may be smaller, check if any of its dimensions
+            // exceeds that of the other box, if so, save the name of those dimensions.
+            if (this.height > height) {
+                maxDimensions.add(HEIGHT);
+            }
+            if (this.width > width) {
+                maxDimensions.add(WIDTH);
+            }
+            if (this.length > length) {
+                maxDimensions.add(LENGTH);
+            }
+        }
+        return maxDimensions;
     }
 }
